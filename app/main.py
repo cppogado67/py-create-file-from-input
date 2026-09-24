ed0b33fc0d3987c5c2472e9e8be5e41f5cdf579f
claro <c_ppogado67@yahoo.com>
2026-09-24 10:38:26 +0800

    02

--- app/main.py
+++ app/main.py
@@ -1,15 +1,14 @@
-def main():
-   filename = input("Enter name of the file: ")
-lines = []
-while True:
-    line = input("Enter new line of content: ")
-    if line == "stop":
-        break
-    lines.append(line)
-with open(f"{filename}.txt", "w") as file:
-    for line in lines:
-        file.write(line + "\n")
-
+def main() -> None:
+    filename = input("Enter name of the file: ")
+    lines = []
+    while True:
+        line = input("Enter new line of content: ")
+        if line == "stop":
+            break
+        lines.append(line)
+    with open(f"{filename}.txt", "w") as file:
+        for line in lines:
+            file.write(line + "\n")
 
 
 if __name__ == "__main__":
